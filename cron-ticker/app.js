7203a48ebc09c383c2d7017402b9ce02bc0caf7b
Juan Carloss Ramirez <juank27ra@gmail.com>
2023-03-09 16:53:09 -0500

    agregando primera imagen

---
+++ cron-ticker/app.js
@@ -0,0 +1,12 @@
+console.log("Hola mundo!");
+
+const cron = require('node-cron');
+
+let times = 0;
+
+cron.schedule('1-59/5 * * * * *', () => {
+	times++;
+  console.log('Tick cada 5 segundos', times);
+});
+
+console.log('Inicio')
